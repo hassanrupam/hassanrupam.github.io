a2d9ed4322e93912d7d6cd44d51c9b3c72bb5340
Hassan Sakib Afrin <hassanrupam@gmail.com>
2023-03-19 13:48:31 +0600

    update vs code and gh-pages

--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function VsCode() {
     return (
-        <iframe src="https://github1s.com/hassanrupam/hassanrupam.github.io/blob/HEAD/components/ubuntu.js" frameBorder="0" title="VsCode" className="h-full w-full bg-ub-cool-grey"></iframe>
+        <iframe src="https://github1s.com/hassanrupam/hassanrupam.github.io/blob/master/components/ubuntu.js" frameBorder="0" title="VsCode" className="h-full w-full bg-ub-cool-grey"></iframe>
         // this is not my work, but it's amazing!
         // Here is the link to the original repo: https://github.com/conwnet/github1s
     )
