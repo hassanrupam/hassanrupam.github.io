145cd9a8fed88390eff71adc4be8abe08572ffc7
Hassan Sakib Afrin <hassanrupam@gmail.com>
2023-03-22 18:05:54 +0600

    Updates

---
+++ _next/static/C9QKWOOgjzdAsBfDijb63/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-6cf11adc33c7fdd4.js","static/chunks/356-f408bb5b9c9cd862.js","static/chunks/pages/index-dca96735830c8089.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
