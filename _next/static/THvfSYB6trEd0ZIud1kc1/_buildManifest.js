19738ab5734dfccafb3648945e63480069a28c27
hassan.rupam <sakib.peoplescape@bkash.com>
2024-06-30 22:32:49 +0600

    Updates

--- _next/static/THvfSYB6trEd0ZIud1kc1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-6cf11adc33c7fdd4.js","static/chunks/8-a0afaf94892de2e4.js","static/chunks/pages/index-618ebb1d5644ccc3.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
